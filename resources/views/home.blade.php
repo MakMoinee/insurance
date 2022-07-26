22891730b4fd8d2993028d6df98ae34746c8f150
Titus Silver <titus.silver.ph@gmail.com>
2022-07-27 00:32:34 +0800

    update code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@ $version = \Config::get('app.appVersion');
 
 <head>
     <link rel="icon" href="/storage/image/favicon.ico" type="image/x-icon">
-    <title>Login 04</title>
+    <title>Welcome to Insurance</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
